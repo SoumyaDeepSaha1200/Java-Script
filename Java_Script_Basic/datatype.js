c4a4b646018454bbfc1ab353180c799752d89558
Soumya Deep saha <111523309+SoumyaDeepSaha1200@users.noreply.github.com>
2024-07-08 11:58:50 +0000

    container config

---
+++ Java_Script_Basic/datatype.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//alert(3 + 5) // We are using nodejs, not browser
+
+console.log(3+3); console.log("Deep") //Code readability alert
+
+console.log(3+3)
+console.log("Deep")
---
+++ Java_Script_Basic/variable.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="deepadiotic@gmail.com"
+var accountPassword="145545455"
+accountCity="Kolkata"
+
+
+//accountId=2
+
+accountEmail="deep@12121gmail.com"
+accountPassword="4545445454"
+accountCity="Delhi"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
