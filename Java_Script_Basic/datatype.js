d07f3f85d8e8f68b6d6f1dd8abc0a87dd908e731
Soumya Deep saha <111523309+SoumyaDeepSaha1200@users.noreply.github.com>
2024-07-08 12:03:40 +0000

    container config

--- Java_Script_Basic/datatype.js
+++ Java_Script_Basic/datatype.js
@@ -1,8 +1,29 @@
-"use strict"
+"use strict"; // treat all JS code as newer version
 
-//alert(3 + 5) // We are using nodejs, not browser
+// alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3+3); console.log("Deep") //Code readability alert
+console.log(3 
+    +
+     3) // code readability should be high
 
-console.log(3+3)
-console.log("Deep")
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
