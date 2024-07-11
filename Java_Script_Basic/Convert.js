635770c80b6f6a85989a949e13085de1fe9b209d
Soumya Deep saha <111523309+SoumyaDeepSaha1200@users.noreply.github.com>
2024-07-11 16:32:21 +0000

    config

---
+++ Java_Script_Basic/Convert.js
@@ -0,0 +1,35 @@
+let score=33
+let score1="55"
+
+console.log(typeof score); //Both are currect for type casting
+console.log(typeof(score)); //Both are currect for type casting
+console.log(typeof score1)
+
+let number=Number(score1); //type chasting 
+console.log(typeof number)
+
+
+let score2="Deep"
+let booleanIslogedIn=Boolean(score2)
+console.log(booleanIslogedIn)
+
+
+
+let somenumber=33
+let Stringof=String(somenumber)
+console.log(Stringof)
+// ************************** Operation **************************
+let value=3
+let negValue= -value
+console.log(value)
+console.log(negValue)
+let str1="Hello"
+let str2=" Hitesh"
+let str3=str1+str2
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2) 
+console.log(1 + 2 + "2")
