f78ba86ae990b1d9123d8341421fc30f255d904e
Soumya Deep saha <111523309+SoumyaDeepSaha1200@users.noreply.github.com>
2024-08-15 09:05:23 +0000

    config

--- Java_Script_Basic_03/controlflow.js
+++ Java_Script_Basic_03/controlflow.js
@@ -1,11 +1,41 @@
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
 
-if (isUserLoggedIn){
-
+if (2!=3){
+    console.log("executed")
 }
 // if (false){
 
 // }
 
-// <, >, <=,>=, ==, !=
+// <, >, <=,>=, ==, !=, ===
 // = this used for assign variable
+
+// const balance = 1000
+// // if (balance > 500) console.log("test")
+
+// if(balance < 500)
+// {
+//     console.log("Less than")
+// }
+// else if(balance < 750)
+// {
+//     console.log("Less than 750")
+// }
+// else if(balance < 900)
+// {
+//     console.log("Less than 900")
+// }
+// else(balance < 1200)
+// {
+//      console.log("Less than 1200")
+// }
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+
+if(userLoggedIn && debitcard && 2==3)
+{
+    console.log("Allow to buy course")
+}
